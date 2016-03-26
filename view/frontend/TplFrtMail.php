9fb3b81f6136c540f960467553fb6291d2425bd8
Agustin Kryger <agustinkryger@gmail.com>
2016-03-26 15:13:02 -0300

    subida de originales y css

--- view/frontend/TplFrtMail.php
+++ view/frontend/TplFrtMail.php
@@ -1,9 +1,10 @@
 <?php
 
-class TplFrtMail{
-    public static function getContactMail($name, $age, $email, $institution, $subject, $message){
-        $oTpl = new UtlTemplate('contact.html', TPL_PATH."mail/");
-        
+class TplFrtMail {
+
+    public static function getContactMail($name, $age, $email, $institution, $subject, $message) {
+        $oTpl = new UtlTemplate('contact.html', TPL_PATH . "mail/");
+
         $oTpl->assign("NAME", $name);
         $oTpl->assign("AGE", $age);
         $oTpl->assign("EMAIL", $email);
@@ -11,6 +12,16 @@ class TplFrtMail{
         $oTpl->assign("SUBJECT", $subject);
         $oTpl->assign("MESSAGE", $message);
         
+        self::assignConstants($oTpl);
+
         return $oTpl->getOutputContent();
     }
+
+    protected static function assignConstants(UtlTemplate $oTpl) {
+        $oTpl->assignGlobal("IMG_URL", IMG_URL);
+        $oTpl->assignGlobal("CSS_URL", CSS_URL);
+        $oTpl->assignGlobal("COMMON_JS_URL", COMMON_JS_URL);
+        $oTpl->assignGlobal("WEB_URL",WEB_PATH);
+    }
+
 }
