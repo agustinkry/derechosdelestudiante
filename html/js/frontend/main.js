6e6a78f53f6603bd3cf7dc6ad80bfee6bf726e35
Pablo Rodriguez <prodriguez@theelectricfactory.com>
2016-04-03 23:33:04 -0300

    Formulario de consulta con sus input

--- html/js/frontend/main.js
+++ html/js/frontend/main.js
@@ -1,3 +1,5 @@
+var $rightOptions;
+
 $(document).ready(function () {
 
     // make sure div stays full width/height on resize
@@ -5,36 +7,64 @@ $(document).ready(function () {
         var winWidth = $(window).width() - 40;
         var winHeight = $(window).height() - 40;
 
-     if (winWidth > 680){
+        if (winWidth > 680) {
 
 
-        $('body').css({
-            'width': winWidth,
-            'height': winHeight
-        });
-        $('html').removeClass("mobile")
+            $('body').css({
+                'width': winWidth,
+                'height': winHeight
+            });
+            $('html').removeClass("mobile")
 
-     } else {
+        } else {
 
-        $('body').css({
-            'width': "auto",
-            'height': "auto"
-        });
-        $('html').addClass("mobile")
+            $('body').css({
+                'width': "auto",
+                'height': "auto"
+            });
+            $('html').addClass("mobile")
 
-     }  
-        
+        }
+
     });
 
     // set initial div height / width
     $(window).resize();
+
+
+    $rightOptions = $("#right select option");
+
+    $("#category").change(function () {
+        var $filteredRights = [];
+        var categoryId = $("#category option:selected").val();
+        if (categoryId != "-1") {
+            $rightOptions.each(function (key, value) {
+                if (key == 0) {
+                    $filteredRights.push($rightOptions[key]);
+                } else {
+                    var dataCategory = $(value).attr("data-category");
+                    if (typeof dataCategory != "undefined") {
+                        $.each(dataCategory.split(","), function (k, v) {
+                            if (v == categoryId) {
+                                $filteredRights.push($rightOptions[key]);
+                            }
+                        });
+                    }
+                }
+            });
+        }else{
+            $filteredRights = $rightOptions;
+        }
+        
+        $("#right select").html($filteredRights);
+    });
 });
 
-$("#search").click(function(){
-      $("nav li input").toggleClass("show");
-  });
+$("#search").click(function () {
+    $("nav li input").toggleClass("show");
+});
 
 
-    // Pretty simple huh?
-    var scene = document.getElementById('elements');
-    var parallax = new Parallax(scene);
+// Pretty simple huh?
+var scene = document.getElementById('elements');
+var parallax = new Parallax(scene);
--- view/frontend/TplFrtContainer.php
+++ view/frontend/TplFrtContainer.php
@@ -14,7 +14,47 @@ class TplFrtContainer {
 
         $this->oTpl->assignGlobal("pageTitle", $title);
         $this->oTpl->assignGlobal("body_classes", $bodyClasses);
-        
+
+        //get all categories
+
+        $oDbaCategory = new DbaFrtCategory();
+        $categories = $oDbaCategory->getAllChildrenCategories();
+
+        foreach ($categories as $oCategory) {
+            $this->oTpl->newBlock("CATEGORY");
+            $this->oTpl->assign("cateogry_id", $oCategory->getId());
+            $this->oTpl->assign("category_name", $oCategory->getName());
+        }
+
+
+        //get all rights
+
+        $oDbaRight = new DbaFrtRight();
+        $rights = $oDbaRight->getRights();
+        foreach ($rights as $oRight) {
+            $this->oTpl->newBlock("RIGHT");
+            $this->oTpl->assign("right_id", $oRight->getId());
+            $this->oTpl->assign("right_title", $oRight->getTitle());
+            $this->oTpl->assign("right_categories", implode(",", $oRight->getCategories()));
+        }
+
+        //get all institutions
+        $oDbaInstitution = new DbaFrtInstitution();
+        $institutions = $oDbaInstitution->getAllInstitutions();
+        foreach ($institutions as $oInstitution) {
+            $this->oTpl->newBlock("INSTITUTION");
+            $this->oTpl->assign("institution_id", $oInstitution->getId());
+            $this->oTpl->assign("institution_name", $oInstitution->getName());
+        }
+
+        //location
+        $locations = UtlLocation::getAllLocations();
+        foreach ($locations as $key => $location) {
+            $this->oTpl->newBlock("LOCATION");
+            $this->oTpl->assign("location_name", $location);
+            $this->oTpl->assign("location_id", $key);
+        }
+
         $this->assignConstants($this->oTpl);
 
         return $this->oTpl->getOutputContent();
