ed10c11791a8668dbe8c521c75380658dfceb386
Pablo Rodriguez <prodriguez@theelectricfactory.com>
2016-04-10 12:12:21 -0300

    test log

--- controller/frontend/ws/inbox.php
+++ controller/frontend/ws/inbox.php
@@ -2,13 +2,13 @@
 include_once '../../../include/include_frontend.php';
 
 $content = var_export($_REQUEST, TRUE);
-
+error_log($content);
 //file_put_contents(ROOT_PATH."log/inbox.txt", $content);
 
-
-$fp = fopen(ROOT_PATH."log/inbox.txt", 'w');
-fwrite($fp, $content);
-fclose($fp);
+//
+//$fp = fopen(ROOT_PATH."log/inbox.txt", 'w');
+//fwrite($fp, $content);
+//fclose($fp);
 
 /* 
  * To change this license header, choose License Headers in Project Properties.
