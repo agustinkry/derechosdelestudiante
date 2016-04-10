2c182e0fb6e6c55db3516cec9c6ed4a5b0793107
Pablo Rodriguez <prodriguez@theelectricfactory.com>
2016-04-10 13:06:58 -0300

    test mail

--- controller/frontend/ws/inbox.php
+++ controller/frontend/ws/inbox.php
@@ -1,8 +1,19 @@
 <?php
+
 include_once '../../../include/include_frontend.php';
 
 $content = var_export($_REQUEST, TRUE);
-error_log($content);
+
+
+$html = TplFrtMail::getContactMail("test", "1", "test", "test", "test", $content);
+$oMail = UtlConfigMail::getConfiguredMail();
+
+$oMail->addAddress("pprodriguez02@gmail.com");
+$oMail->Subject =  "Consulta sin asunto";
+$oMail->Body = $html;
+
+$oMail->send();
+
 //file_put_contents(ROOT_PATH."log/inbox.txt", $content);
 
 //
