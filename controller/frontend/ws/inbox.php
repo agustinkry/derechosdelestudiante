783613d8fb8453ec01da74abe0bee159b3fa179e
Pablo Rodriguez <prodriguez@theelectricfactory.com>
2016-04-10 12:08:03 -0300

    test inbox

---
+++ controller/frontend/ws/inbox.php
@@ -0,0 +1,18 @@
+<?php
+include_once '../../../include/include_frontend.php';
+
+$content = var_export($_REQUEST, TRUE);
+
+//file_put_contents(ROOT_PATH."log/inbox.txt", $content);
+
+
+$fp = fopen(ROOT_PATH."log/inbox.txt", 'w');
+fwrite($fp, $content);
+fclose($fp);
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
