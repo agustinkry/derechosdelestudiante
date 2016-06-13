ee2ef7c41d21caa1bbcebc42f95a354d92549c2b
Pablo Rodriguez <prodriguez@theelectricfactory.com>
2016-06-13 00:03:45 -0300

    correcion zona horaria

--- include/lib/ssp.class.php
+++ include/lib/ssp.class.php
@@ -384,7 +384,7 @@ class SSP {
 				$sql_details['pass'],
 				array( PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8' )
 			);
-                        $db->exec("SET time_zone = '".date_default_timezone_get()."'");
+                        $db->exec("SET time_zone = '-03:00'");
 		}
 		catch (PDOException $e) {
 			self::fatal(
